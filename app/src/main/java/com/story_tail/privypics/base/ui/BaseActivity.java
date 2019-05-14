667ae4c4dd538439015a494d90a596ceb3a7bf95
Gyasi Story <gyasistory@gmail.com>
2019-05-14 07:26:16 -0400

    Update BaseActivity.java

--- app/src/main/java/com/story_tail/privypics/base/ui/BaseActivity.java
+++ app/src/main/java/com/story_tail/privypics/base/ui/BaseActivity.java
@@ -1,18 +1,28 @@
 package com.story_tail.privypics.base.ui;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
-    protected FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    protected FirebaseAuth  mAuth;
+    protected FirebaseUser currentUser;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        mAuth = FirebaseAuth.getInstance();
+    }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        currentUser = mAuth.getCurrentUser();
 
     }
 }
