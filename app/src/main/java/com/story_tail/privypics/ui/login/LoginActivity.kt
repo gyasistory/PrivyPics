6a2b97ffef01b72a09a628ed18183952b6a9feb1
Gyasi Story <gyasistory@gmail.com>
2019-05-14 07:26:57 -0400

    Change to base Activity

--- app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
+++ app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
-import androidx.appcompat.app.AppCompatActivity
 import com.story_tail.privypics.R
+import com.story_tail.privypics.base.ui.BaseActivity
 
 
-class LoginActivity : AppCompatActivity() {
-
+class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +19,7 @@ class LoginActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.password)
         val login = findViewById<Button>(R.id.login)
         val loading = findViewById<ProgressBar>(R.id.loading)
+
 
     }
 }
