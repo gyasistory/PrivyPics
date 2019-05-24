943c64b8b8e0b618d480893237fdb94665bb6bd3
Gyasi Story <gyasi.story@cru.org>
2019-05-24 18:23:03 -0400

    UPDATE

--- app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
+++ app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
@@ -7,21 +7,18 @@ import android.widget.ProgressBar
 import com.crashlytics.android.Crashlytics
 import com.story_tail.privypics.R
 import com.story_tail.privypics.base.ui.BaseActivity
+import com.story_tail.privypics.databinding.ActivityLoginBinding
 import io.fabric.sdk.android.Fabric
 
 
 class LoginActivity : BaseActivity() {
 
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_login)
-        Fabric.with(this, Crashlytics())
-        val username = findViewById<EditText>(R.id.username)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<Button>(R.id.login)
-        val loading = findViewById<ProgressBar>(R.id.loading)
-
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
