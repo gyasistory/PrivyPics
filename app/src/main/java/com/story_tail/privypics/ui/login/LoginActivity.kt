28c00e27052e4ea81d1df53998e42e90a4c0289e
Gyasi Story <gyasistory@gmail.com>
2019-05-24 20:35:36 -0400

    updates

--- app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
+++ app/src/main/java/com/story_tail/privypics/ui/login/LoginActivity.kt
@@ -1,19 +1,31 @@
 package com.story_tail.privypics.ui.login
 
 import android.os.Bundle
+import android.widget.Toast
 import com.story_tail.privypics.base.ui.BaseActivity
 import com.story_tail.privypics.databinding.ActivityLoginBinding
+import com.story_tail.privypics.ui.login.viewmodel.LogInListener
+import com.story_tail.privypics.ui.login.viewmodel.LogInViewModel
 
 
-class LoginActivity : BaseActivity() {
+class LoginActivity : BaseActivity(), LogInListener {
 
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding.viewModel = LogInViewModel(this)
         setContentView(binding.root)
+    }
 
+    override fun onLogInClick(email: String?, password: String?) {
+        Toast.makeText(
+            this,
+            String.format("Email => %s \n Password => %s", email, password),
+            Toast.LENGTH_LONG
+        ).show()
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
