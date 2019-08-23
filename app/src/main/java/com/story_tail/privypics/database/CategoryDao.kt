9aeb624292f34204096c69f41b9b670ad5ebd38d
Gyasi Story <gyasi.story@cru.org>
2019-08-23 17:22:52 -0400

    update database

--- app/src/main/java/com/story_tail/privypics/database/CategoryDao.kt
+++ app/src/main/java/com/story_tail/privypics/database/CategoryDao.kt
@@ -7,14 +7,14 @@ import com.story_tail.privypics.model.Category
 @Dao
 interface CategoryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertCategory(vararg category: Category)
+    suspend fun insertCategory(vararg category: Category)
 
     @Update
-    fun updateCategory(vararg category: Category)
+    suspend fun updateCategory(vararg category: Category)
 
     @Delete
-    fun deleteCategory(vararg category: Category)
+    suspend fun deleteCategory(vararg category: Category)
 
     @Query("SELECT * from category_table ORDER BY name DESC")
-    fun getAllCategorys(): LiveData<List<Category>>
+    fun getAllCategories(): LiveData<List<Category>>
 }
