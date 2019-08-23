f61c977c62fb58951c4cc2a4b0651e64990b5876
Gyasi Story <gyasi.story@cru.org>
2019-08-23 10:20:37 -0400

    Create Models

--- app/src/main/java/com/story_tail/privypics/model/Image.kt
+++ app/src/main/java/com/story_tail/privypics/model/Image.kt
@@ -1,2 +1,18 @@
 package com.story_tail.privypics.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "image_table")
+class Image {
+    @PrimaryKey(autoGenerate = true)
+    var id: UUID = UUID.randomUUID()
+
+    @ColumnInfo(typeAffinity = ColumnInfo.BLOB, name = "image_data")
+    var data: ByteArray? = null
+
+    @ColumnInfo(name = "image_description")
+    var description: String? = null
+}
