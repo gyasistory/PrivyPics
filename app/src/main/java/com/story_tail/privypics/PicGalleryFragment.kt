65d37b84f327de21a707cedae1ea58cde18b5544
Gyasi Story <gyasi.story@cru.org>
2019-08-23 14:38:48 -0400

    Update PicGalleryFragment.kt

--- app/src/main/java/com/story_tail/privypics/PicGalleryFragment.kt
+++ app/src/main/java/com/story_tail/privypics/PicGalleryFragment.kt
@@ -1,12 +1,10 @@
 package com.story_tail.privypics
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +15,7 @@ private const val ARG_PARAM2 = "param2"
  * A simple [Fragment] subclass.
  *
  */
-class PicGallaryFragment : Fragment() {
+class PicGalleryFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +24,4 @@ class PicGallaryFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pic_gallary, container, false)
     }
-
-
 }
