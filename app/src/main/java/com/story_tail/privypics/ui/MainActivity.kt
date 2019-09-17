bad45714206269bf34196a6d5c70f623e15c81c0
Gyasi Story <gyasi.story@cru.org>
2019-09-17 15:26:34 -0400

    code clean up

--- app/src/main/java/com/story_tail/privypics/ui/MainActivity.kt
+++ app/src/main/java/com/story_tail/privypics/ui/MainActivity.kt
@@ -80,18 +80,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-////        return when (item.itemId) {
-//////            R.id.action_settings -> true
-////            else -> super.onOptionsItemSelected(item)
-////        }
-//        return super.onOptionsItemSelected(item)
-//    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
