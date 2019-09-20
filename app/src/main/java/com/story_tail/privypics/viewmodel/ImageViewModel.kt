96937fc320af6dbfa0248107572fa6ecc09995bc
Gyasi Story <gyasi.story@cru.org>
2019-09-20 15:16:40 -0400

    add coroutine

--- app/src/main/java/com/story_tail/privypics/viewmodel/ImageViewModel.kt
+++ app/src/main/java/com/story_tail/privypics/viewmodel/ImageViewModel.kt
@@ -2,27 +2,44 @@ package com.story_tail.privypics.viewmodel
 
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.story_tail.privypics.model.Image
 import com.story_tail.privypics.repository.ImageRepository
-import kotlinx.coroutines.coroutineScope as coroutineScope1
+import kotlinx.coroutines.launch
+import java.util.*
 
 class ImageViewModel(context: Context) : ViewModel() {
     private val imageRepository by lazy { ImageRepository(context) }
-    private var model: Image? = Image()
+    private var model = MutableLiveData<Image>()
 
     fun setImage(image: Image?) {
-        model = image
+        model.value = image
     }
 
     fun saveImage() {
+        viewModelScope.launch {
+            model.value?.let { imageRepository.insertImage(it) }
+        }
 
-            model?.let { coroutineScope1 {  imageRepository.insertImage(it) } }
     }
 
     var image: Bitmap?
         set(value) {
-            model?.data = value
+            model.value?.let {
+                it.data = value
+                return
+            }
+            val image = createImage()
+            image.data = value
+            model.value = image
         }
-        get() = model?.data
+        get() = model.value?.data
+
+    private fun createImage(): Image {
+        val image = Image()
+        image.id = UUID.randomUUID()
+        return image
+    }
 }
